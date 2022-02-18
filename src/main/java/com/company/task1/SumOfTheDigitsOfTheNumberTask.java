692f78f2e3f67ab1699527125f278d46109a3cf4
Andrei Zharikov <andreykazharik@gmail.com>
2022-02-18 07:50:27 +0300

    Add new view method and new filling array method

--- src/main/java/com/company/task1/SumOfTheDigitsOfTheNumberTask.java
+++ src/main/java/com/company/task1/SumOfTheDigitsOfTheNumberTask.java
@@ -7,13 +7,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SumOfTheDigitsOfTheNumberTask {
+    private static final int firstMagicNumber = 0;
+    private static final int secondMagicNumber = 99;
 
     public void logic() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(bf.readLine());
         SumOfTheDigitsOfTheNumber sumOfTheDigitsOfTheNumber = new SumOfTheDigitsOfTheNumber();
-        int sumOfAllDigits = sumOfTheDigitsOfTheNumber.getSumOfTheDigitsOfTheNumber(number, 0);
-        int sumOfTheLastTwoDigits = sumOfTheDigitsOfTheNumber.getSumOfTheDigitsOfTheNumber(number, 99);
+        int sumOfAllDigits = sumOfTheDigitsOfTheNumber.getSumOfTheDigitsOfTheNumber(number, firstMagicNumber);
+        int sumOfTheLastTwoDigits = sumOfTheDigitsOfTheNumber.getSumOfTheDigitsOfTheNumber(number, secondMagicNumber);
         int sumOfTheFirstTwoDigits = sumOfAllDigits - sumOfTheLastTwoDigits;
         ViewTask.showResult(Boolean.toString(sumOfTheFirstTwoDigits == sumOfTheLastTwoDigits));
     }
