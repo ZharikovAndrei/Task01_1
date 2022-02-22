36df9fb075c8396878a2b98e4b4746a209b35901
Andrei Zharikov <andreykazharik@gmail.com>
2022-02-22 16:12:54 +0300

    No

--- src/main/java/com/company/task8/SumOfRequiredElements.java
+++ src/main/java/com/company/task8/SumOfRequiredElements.java
@@ -4,9 +4,7 @@ import com.company.util.data.ConsoleDataAcquirer;
 import com.company.util.data.FillingArray;
 import com.company.util.view.ViewTask;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SumOfRequiredElements {
 
